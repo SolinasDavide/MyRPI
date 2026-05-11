678401811e03da5ff3bc26f2f8aebc18c46ee966
SolinasDavide <davide.solinas.00@gmail.com>
2026-05-11 13:57:04 +0200

    nuovo script in c

---
+++ hello.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello da Windows a Raspberry Pi!\n");
+    
+    getchar();// Questa funzione aspetta che l'utente prema il tasto INVIO
+    return 0;
+}
